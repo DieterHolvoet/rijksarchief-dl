315c3ac750b3b25751f14e1c07295b70234a0980
Dieter Holvoet <dieter.holvoet@gmail.com>
2017-06-19 00:02:13 +0200

    Fixes

--- src/ArchiveDocument.js
+++ src/ArchiveDocument.js
@@ -16,9 +16,14 @@ class ArchiveDocument {
      * @example http://search.arch.be/imageserver/getpic.php?510/510_1546_000/510_1546_000_01852_000/510_1546_000_01852_000_0_0001.jp2&249
      */
     static parseUrl(val) {
-        const result = Array.from(/([0-9]{3})_([0-9]{4})_([0-9]{3})_([0-9]{5})_([0-9]{3})_([0-9]{1})_([0-9]{4})/.exec(val));
-        result.shift();
-        return result;
+        const result = /([0-9]{3})_([0-9]{4})_([0-9]{3})_([0-9]{5})_([0-9]{3})_([0-9]{1})_([0-9]{4})/.exec(val);
+        if (!result) {
+            throw new Error('Error while parsing the url: no document ID found.')
+        }
+
+        const values = Array.from(result);
+        values.shift();
+        return values;
     }
 
     fif(num) {
